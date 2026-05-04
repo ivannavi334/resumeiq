a8609bcde99588f8f5a7a932ae85e2e8ffe7f43e
ivannavi334 <killallxxx3@gmail.com>
2026-05-04 12:02:24 +0300

    Remove email confirmation requirement on registration

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -23,7 +23,6 @@ export function RegisterPage() {
           <Auth
             supabaseClient={supabase}
             view="sign_up"
-            redirectTo={`${window.location.origin}/auth/callback`}
             appearance={{
               theme: ThemeSupa,
               variables: {
